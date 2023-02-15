23c7f2db048e30d29ebb695d0c7225764c47204f
qmigo <ankur031100@gmail.com>
2023-02-15 20:54:10 +0530

    testing router and setting components

---
+++ client/src/components/Recovery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recovery() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recovery
---
+++ client/src/components/Username.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Username() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Username
